cef292cf9bd8b59184501a4ab04cd2d6fa2b2236
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2023-03-04 19:17:57 +1100

    Add footer to app

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Tic Tac Toe App</h1>
+      <p>test</p>
       <footer>
-        This project was coded by <a>Lilish Warrior</a>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/Lilish-Warrior/tic-tac-toe-react"
+          target="_blank"
+        >
+          Lilish Warrior
+        </a>
       </footer>
     </div>
   );
