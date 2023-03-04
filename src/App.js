825788187bc07c58f0b131ca61d5755e4382a29c
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2023-03-04 20:30:21 +1100

    Added logic to set winner

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import "./App.css";
 import { Board } from "./components/Board";
 
 export default function App() {
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
   const [boardList, setBoard] = useState(Array(9).fill(null));
   const [xPlaying, setXPlaying] = useState(true);
 
@@ -14,9 +25,26 @@ export default function App() {
         return value;
       }
     });
+    checkWinner(updateBoard);
     setBoard(updateBoard);
     setXPlaying(!xPlaying);
   };
+
+  const checkWinner = (boardList) => {
+    for (let i = 0; i < WIN_CONDITIONS.length; i++) {
+      const [x, y, z] = WIN_CONDITIONS[i];
+
+      if (
+        boardList[x] &&
+        boardList[x] === boardList[y] &&
+        boardList[y] === boardList[z]
+      ) {
+        console.log(boardList[x]);
+        return boardList[x];
+      }
+    }
+  };
+
   return (
     <div className="App">
       <h1>Tic Tac Toe App</h1>
--- src/components/Board.js
+++ src/components/Board.js
@@ -6,7 +6,9 @@ export const Board = ({ boardList, onClick }) => {
   return (
     <div className="board">
       {boardList.map((value, idx) => {
-        return <Box value={value} onClick={() => onClick(idx)} />;
+        return (
+          <Box value={value} onClick={() => value === null && onClick(idx)} />
+        );
       })}
     </div>
   );
