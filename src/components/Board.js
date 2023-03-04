b5d1b4370c482f67b8650ef9e4fc6d63ea67446c
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2023-03-04 20:13:02 +1100

    Styled board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Board.css";
 import { Box } from "./Box";
 
 export const Board = ({ boardList, onClick }) => {
   return (
-    <div>
+    <div className="board">
       {boardList.map((value, idx) => {
         return <Box value={value} onClick={() => onClick(idx)} />;
       })}
