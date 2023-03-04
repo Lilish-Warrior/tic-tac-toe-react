89ee914472b3a885c420dc9a56f62676f61023b0
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2023-03-04 17:22:22 +1100

    Created app skeleton

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe App</h1>
+      <footer>
+        This project was coded by <a>Lilish Warrior</a>
+      </footer>
+    </div>
+  );
+}
